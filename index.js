936a2223801dbe910b6012f375f6f55680384b2e
Amakine <50359104+Amakine@users.noreply.github.com>
2021-03-12 13:26:57 +0200

    uibuild

--- index.js
+++ index.js
@@ -23,118 +23,103 @@ app.use(morgan(':method :url :status - :res_body - :response-time ms'))
 
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const personID = request.params.id
+  const personID = request.params.id
 
-    Person.findById(personID).then(person => {
-        if (person) response.json(person)
-        else        response.status(404).end()
-    })
+  Person.findById(personID).then(person => {
+    if (person) response.json(person)
+    else        response.status(404).end()
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const personID = request.params.id
-    Person.findByIdAndRemove(personID)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  const personID = request.params.id
+  Person.findByIdAndRemove(personID)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const createPersonObject = (requestBody) => {
-
-    if (!requestBody) {
-        throw {error: 'Request body must contain person in JSON format'}
-    }
-
-    const {name, number} = requestBody
-
-    if (!name || typeof name !== 'string') {
-        throw {error: 'Field "name" must be a non-empty string'}
-    }
-
-    if (!number || typeof number !== 'string') {
-        throw {error: 'Field "number" must be a non-empty string'}
-    }
-
-    const person = {name, number}
-    return person
-}
-
 app.put('/api/persons/:id', (request, response, next) => {
-    console.log('YEEET')
-    let person
-    try {
-        person = createPersonObject(request.body)
-    }
-    catch(error) {
-        response.status(400).json(error)
-        return
-    }
 
-    const personID = request.params.id
-    console.log(personID)
-    Person.findByIdAndUpdate(personID, person, {new: true}) /*, runValidators: true, context: 'query'*/
-        .then(result => {
-            response.status(200).json(result)
-        })
-        .catch(error => next(error))
+  const personID = request.params.id
+  const personData = {
+    name: request.body.name,
+    number: request.body.number
+  }
+  
+  const validationError = (new Person(personData)).validateSync()
+  if (validationError) {
+    return response.status(400).json({
+      error: validationError.message
+    })
+  }
+
+  Person.findByIdAndUpdate(personID, personData, {new: true}) /*, runValidators: true, context: 'query'*/
+    .then(result => {
+      response.status(200).json(result)
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (request, response, next) => {
 
-    const {name, number} = request.body
-    const person = new Person({name, number})
+  const {name, number} = request.body
+  const person = new Person({name, number})
 
-    person.save().then(savedPerson => {
-        console.log(typeof savedPerson);
-        response.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    console.log(typeof savedPerson)
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 
-app.get('/info', (request, response) => {
-    const page =
-    `<!DOCTYPE html>
-    <html><body>
-    <p>
-        Phonebook has info for ${persons.length} people
-        <br>
-        ${(new Date()).toString()}
-    </p>
-    </body></html>`
-    
-    response.setHeader('Content-Type', 'text/html')
-    response.send(page)
+app.get('/info', (req, res) => {
+  Person.count({})
+    .then(personCount => {
+      res.status(200)
+      res.setHeader('Content-Type', 'text/html')
+      res.send(`<p>
+          Phonebook has info for ${personCount} people
+          <br>
+          ${(new Date()).toString()}
+        </p>`)
+    })
+    .catch(error => {
+      res.status(500)
+      res.setHeader('Content-Type', 'text/html')
+      res.send('<p>Accessing person count failed.</p>')
+    })
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
 
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({error: 'unknown endpoint'})
 }
 const errorHandler = (error, request, response, next) => {
-    console.log('Error handler: ', error.message) 
+  console.log('Error handler: ', error.message) 
     
-    if (error.name === 'CastError') {
-        return response.status(400).json({error: 'malformatted id'})
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).json({error: 'malformatted id'})
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
+  next(error)
 }
 
 app.use(unknownEndpoint)
--- models/person.js
+++ models/person.js
@@ -1,49 +1,49 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 })
-.then(result => {
+  .then(result => {
     console.log('connected to MongoDB')
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true
-    }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  }
 }, {
-    pluralize: false
+  pluralize: false
 })
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
   
-const Person = mongoose.model('Person', personSchema, "persons")
+const Person = mongoose.model('Person', personSchema, 'persons')
 module.exports = Person
